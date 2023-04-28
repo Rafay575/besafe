788cb288aeb7fdbffd25a7051fe4b98c605f6899
Kashif Khan <kashifbmk@gmail.com>
2023-04-29 01:25:14 +0500

    removed target link from nav bar

--- resources/views/layouts/partials/site_nav.blade.php
+++ resources/views/layouts/partials/site_nav.blade.php
@@ -2,7 +2,7 @@
   <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="#" target="_blank">
+      <a class="navbar-brand m-0" href="#">
         <img src="{{asset('website/img/logo.png')}}" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold">CMS</span>
       </a>
@@ -11,7 +11,7 @@
     <div class="collapse navbar-collapse  w-auto h-auto" id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('dashboard')) ? 'active' : '' }}" href="#" target="_blank">
+          <a class="nav-link {{ (request()->is('dashboard')) ? 'active' : '' }}" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-shop text-primary text-sm opacity-10"></i>
             </div>
@@ -46,7 +46,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('unsafe-behaviors')) ? 'active' : '' }}" href="{{route('unsafe-behaviors.index')}}" target="_blank" >
+          <a class="nav-link {{ (request()->is('unsafe-behaviors')) ? 'active' : '' }}" href="{{route('unsafe-behaviors.index')}}" >
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Unsafe-Behaviour.ico')}}" alt="" width="18">
             </div>
@@ -55,7 +55,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('hazards')) ? 'active' : '' }}" href="{{route('hazards.index')}}" target="_blank">
+          <a class="nav-link {{ (request()->is('hazards')) ? 'active' : '' }}" href="{{route('hazards.index')}}">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Unsafe-Behaviour.ico')}}" alt="" width="18">
             </div>
@@ -64,7 +64,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('near-miss')) ? 'active' : '' }}" href="{{route('near-miss.index')}}" target="_blank">
+          <a class="nav-link {{ (request()->is('near-miss')) ? 'active' : '' }}" href="{{route('near-miss.index')}}">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Near Miss.ico')}}" alt="" width="18">
             </div>
@@ -73,7 +73,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('fire-property')) ? 'active' : '' }}" href="{{route('fire-property.index')}}" target="_blank">
+          <a class="nav-link {{ (request()->is('fire-property')) ? 'active' : '' }}" href="{{route('fire-property.index')}}">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Fire Property Damage.ico')}}" alt="" width="18">
             </div>
@@ -81,7 +81,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="{{route('injuries.index')}}" target="_blank">
+          <a class="nav-link " href="{{route('injuries.index')}}">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Injuries.ico')}}" alt="" width="18">
             </div>
@@ -91,7 +91,7 @@
 
 
         <li class="nav-item">
-          <a class="nav-link" href="#" target="_blank">
+          <a class="nav-link" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Permission-to-Work-System.ico')}}" alt="" width="18">
             </div>
@@ -100,7 +100,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="#" target="_blank">
+          <a class="nav-link" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Internal External Audit Closures.ico')}}" alt="" width="18">
             </div>
@@ -108,7 +108,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" target="_blank">
+          <a class="nav-link" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Management Walk Through Audits.ico')}}" alt="" width="18">
             </div>
@@ -117,7 +117,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" target="_blank">
+          <a class="nav-link" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Internal External Audit Closures.ico')}}" alt="" width="18">
             </div>
@@ -125,7 +125,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link {{ (request()->is('meta-data')) ? 'active' : '' }}" href="{{route('meta-data.index')}}" target="_blank">
+          <a class="nav-link {{ (request()->is('meta-data')) ? 'active' : '' }}" href="{{route('meta-data.index')}}">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Meta-Data.ico')}}" alt="" width="18">
             
@@ -134,7 +134,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="" target="_blank">
+          <a class="nav-link" href="">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               <img src="{{asset('website/nav_icons/Reports.ico')}}" alt="" width="18">
             </div>
@@ -142,7 +142,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#" target="_blank">
+          <a class="nav-link" href="#">
             <div class="icon icon-shape icon-sm text-center  me-2 d-flex align-items-center justify-content-center">
               {{-- <i class="text-primary text-sm opacity-10">
 
