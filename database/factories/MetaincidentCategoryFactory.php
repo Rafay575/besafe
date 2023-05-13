407d4b9d39c297d20db213e64cdc8ef807e36f6f
Kashif Khan <kashifbmk@gmail.com>
2023-05-13 22:24:07 +0500

    meta incidnet category fixed

--- database/factories/MetaincidentCategoryFactory.php
+++ database/factories/MetaincidentCategoryFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\MetaIncidentCategory>
  */
-class MetaincidentCategoryFactory extends Factory
+class MetaIncidentCategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
