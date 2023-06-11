1d59a68c07ede10756f1067049e0b76726c3c126
Kashif Khan <kashifbmk@gmail.com>
2023-06-12 00:49:26 +0500

    report updated

--- resources/views/pdfreports/partials/action-buttons.blade.php
+++ resources/views/pdfreports/partials/action-buttons.blade.php
@@ -2,5 +2,5 @@
     <td class="text-sm">
         <x-forms.action-btn href="{{asset('reports/' . $report['file_name'])}}" action="download" title="download report"></x-forms.action-btn>
         <x-forms.action-btn href="" id="table_data_delete" action="delete" title="delete report"
-            data-action="{{route('reports.destroy',$report['id'])}}" data-parent="tr"></x-forms.action-btn>
+            data-action="{{route('pdfreports.destroy',$report['id'])}}" data-parent="tr"></x-forms.action-btn>
     </td>
