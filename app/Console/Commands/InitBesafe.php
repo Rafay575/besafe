b456f5049b2b7eb0467fa379f041ab7d670331b4
Kashif Khan <kashifbmk@gmail.com>
2023-07-07 01:04:00 +0500

    bug fixes

--- app/Console/Commands/InitBesafe.php
+++ app/Console/Commands/InitBesafe.php
@@ -28,7 +28,7 @@ class InitBesafe extends Command
         $this->call('migrate:fresh');
         $this->call('db:seed', ['--class' => 'UserRolesAndPermissionSeeder']);
         $this->call('db:seed', ['--class' => 'DefaultUserSeeder']);
-        // $this->call('db:seed', ['--class' => 'MetaDataSeeder']);
+        $this->call('db:seed', ['--class' => 'MetaDataSeeder']);
         $this->call('db:seed', ['--class' => 'AboutSeeder']);
         // $this->call('storage:link');
     }
